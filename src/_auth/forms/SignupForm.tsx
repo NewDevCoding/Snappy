948522c3cb0a90a50520f1100034aa952aea69e3
Isaak <isaac@thesheridans.email>
2023-12-09 09:43:50 -0800

    commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,10 +1,12 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
+
 
 import {
   Form,
   FormControl,
-  FormDescription,
+  // FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,11 +17,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/srccomponents/ui/button";
 import { useForm } from "react-hook-form";
 import { signupValidation } from "@/srclib/validation";
+import Loader from "C:/Users/isaac/Desktop/Snappy/src/components/shared/Loader.tsx"
+import { createUserAccount } from "@/srclib/appwrite/api";
 // import { z } from "zod";
 
 
 const SignupForm = () => {
-  
+  const isLoading = false;
 
   const form = useForm<z.infer<typeof signupValidation>>({
     resolver: zodResolver(signupValidation),
@@ -31,36 +35,88 @@ const SignupForm = () => {
     },
   })
 
-  function onSubmit(values: z.infer<typeof signupValidation>) {
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof signupValidation>) {
+    const newUser = await createUserAccount(values);
+    
+    console.log(newUser)
   }
 
   return (
       <Form {...form}>
         <div className="sm:w-420 flex-center flex-col">
-          <img src="/assets/images/profile.png" alt="logo"></img>
-        </div>
+          <img src="/assets/images/logo.svg" alt="logo"></img>
+
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+          <p className="text-light-3 small-medium md:base-regular">To use Snapgram, please enter your details</p>
 
 
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>username</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input type="text" className="shad-input" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader/> Loading...
+              </div>
+            ): "Sign up"}
+          </Button>
+
+          <p className="text-small-regular text-light-2 text-center">
+            Already have an account?
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          </p>
         </form>
+        </div>
       </Form>
   )
 }
