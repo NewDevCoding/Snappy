5908a677a18673a01c05d9f868b6f6dd0058d4f0
Isaak <isaac@thesheridans.email>
2024-01-05 12:24:55 -0800

    created mutation for useSignInAccount in queriesAndMutations file

--- src/srclib/react-query/queriesAndMutations.ts
+++ src/srclib/react-query/queriesAndMutations.ts
@@ -8,8 +8,17 @@ import {
 import { createUserAccount } from '../appwrite/api'
 
 // initialized new mutation function so query knows what we are doing
-export const useCreateUserAccountMutation = () => {
+export const useCreateUserAccount = () => {
     return useMutation({
         mutationFn: (user: INewUser) => createUserAccount(user)
+    })
+}
+
+export const useSignInAccount = () => {
+    return useMutation({
+        mutationFn: (user: { 
+            email: string; 
+            password: string
+        }) => signInAccount(user)
     })
 }
