dbcde60c68cf8262b984b637e12b36eeaa87ffd6
Isaak <isaac@thesheridans.email>
2024-01-09 20:39:45 -0800

    import

--- src/srclib/react-query/queriesAndMutations.ts
+++ src/srclib/react-query/queriesAndMutations.ts
@@ -5,7 +5,7 @@ import {
     useQueryClient,
     useInfiniteQuery
 } from '@tanstack/react-query'
-import { createUserAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount } from '../appwrite/api'
 
 // initialized new mutation function so query knows what we are doing
 export const useCreateUserAccount = () => {
