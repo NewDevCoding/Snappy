f2f5938c685b08f8a76ae718f59c08a87235f673
Isaak <isaac@thesheridans.email>
2024-01-19 20:38:34 -0800

    created left side bar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,6 +1,61 @@
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/srclib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
+
 const LeftSidebar = () => {
+
+    const { mutate: signOut, isSuccess } = useSignOutAccount();
+    const navigate = useNavigate();
+    const { user } = useUserContext();
+
+    useEffect(() => {
+        if (isSuccess) {
+            navigate(0);
+        }
+    }, [isSuccess])
     return (
-      <div>LeftSidebar</div>
+      <nav className="leftsidebar">
+        <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+                <img 
+                    src="/assets/images/logo.svg" 
+                    alt="logo" 
+                    width={170}
+                    height={36}
+                />
+        </Link>
+        <Link to={`/profile/${user.id}`} className="flrx gap-3 items-center">
+          <img src={user.imageURL || "/assets/icons/profile.svg"} alt="profile" className="h-14 w-14 rounded-full" />
+          <div className="flex flex-col">
+            <p className="body-bold">
+              {user.name}
+            </p>
+            <p className="small-regular text-light-3">
+              @{user.username}
+            </p>
+          </div>
+        </Link>
+
+        {/*<ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            return (
+              <li 
+              key={link.label} 
+              className="leftsidebar-link"
+              >
+                <NavLink to={link.route}>
+                  {link.label}
+                </NavLink>
+              </li>
+            )
+          })}
+        </ul> */}
+        </div>
+      </nav>
     )
 }
 
