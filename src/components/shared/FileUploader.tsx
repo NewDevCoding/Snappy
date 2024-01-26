1fffaeb3413ee3ac077a008c3389173dece282c5
Isaak <isaac@thesheridans.email>
2024-01-26 11:26:06 -0800

    finished fileUploader

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,18 +1,28 @@
 import React, { useCallback, useState } from 'react';
-import { useDropzone } from 'react-dropzone';import { Button } from '../ui/button';
+import { useDropzone, FileWithPath } from 'react-dropzone';import { Button } from '../ui/button';
 ;
 
-const FileUploader = () => {
+type FileUploaderProps = {
+  fieldChange: (FILES: File[]) => void;
+  mediaUrl: string;
+}
 
-    const [fileUrl, setfileUrl] = useState('')
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => 
+{
+    const [ file, setFile ] = useState<File[]>([]);
+    const [fileUrl, setFileUrl] = useState('');
 
-    const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles);
-    }, [])
+    const onDrop = useCallback(
+      (acceptedFiles: FileWithPath[]) => {
+        setFile(acceptedFiles);
+        fieldChange(acceptedFiles);
+        setFileUrl(URL.createObjectURL(acceptedFiles[0]))
+    }, [file])
+
     const {getRootProps, getInputProps } = useDropzone({
       onDrop,
       accept: {
-        'image/*': ['.png', '.jpeg', '.jpg']
+        'image/*': ['.png', '.jpeg', '.jpg', '.svg']
       }
     })
 
@@ -21,9 +31,16 @@ const FileUploader = () => {
       <input {...getInputProps()} className='cursor-pointer' />
       {
       fileUrl? (
-        <div>
-            test 1
+        <>
+        <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+           <img 
+              src={fileUrl}
+              alt='image'
+              className='file_uploader-img'
+          />
         </div>
+        <p className='file_uploader-label'>Click or drag photo to replace</p>
+        </>
       ) : (
         <div className='file_uploader-box'>
             <img 
