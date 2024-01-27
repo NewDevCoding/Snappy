81a45fbea2838271f0b94b41d3f27a38c28fc284
Isaak <isaac@thesheridans.email>
2024-01-27 12:12:33 -0800

    fixed import statement for QUERY_KEYS and removed static statement

--- src/srclib/react-query/queriesAndMutations.ts
+++ src/srclib/react-query/queriesAndMutations.ts
@@ -6,28 +6,9 @@ import {
     useInfiniteQuery
 } from '@tanstack/react-query'
 import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-//  import { QUERY_KEYS } from './queryKeys'
+import { QUERY_KEYS } from './queryKeys'
 
- export enum QUERY_KEYS {
-     // AUTH KEYS
-     CREATE_USER_ACCOUNT = "createUserAccount",
-  
-     // USER KEYS
-     GET_CURRENT_USER = "getCurrentUser",
-     GET_USERS = "getUsers",
-     GET_USER_BY_ID = "getUserById",
-  
-     // POST KEYS
-     GET_POSTS = "getPosts",
-     GET_INFINITE_POSTS = "getInfinitePosts",
-     GET_RECENT_POSTS = "getRecentPosts",
-     GET_POST_BY_ID = "getPostById",
-     GET_USER_POSTS = "getUserPosts",
-     GET_FILE_PREVIEW = "getFilePreview",
-  
-     //  SEARCH KEYS
-     SEARCH_POSTS = "getSearchPosts",
-   }
+ 
 
 
 
@@ -68,5 +49,6 @@ export const useSignOutAccount = () => {
      })
 
  }
+
 
  
