0273a4cad4facdff5fb2eaca4a0969b1a42d9cff
Isaak <isaac@thesheridans.email>
2024-02-16 14:30:41 -0800

    created boilerplate for home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,26 @@
-
+import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "C:/Users/isaac/Desktop/Snappy/src/srclib/react-query/queriesAndMutations.ts";
 
 const Home = () => {
+
+  const { data: posts, isPending: isPostLoading, isError: isErrorPosts} = useGetRecentPosts()
+
   return (
-    <div>Home</div>
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">
+            Home Feed</h2>
+            {isPostLoading && !posts ? (
+              <Loader/>
+            ) : (
+              <ul>
+                test
+              </ul>
+            )}
+        </div>
+      </div>
+    </div>
   )
 }
 
