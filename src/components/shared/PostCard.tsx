0e6b0e5af9eee7d716ac9ff336b2415ff8ae4415
Isaak <isaac@thesheridans.email>
2024-02-17 14:29:38 -0800

    fixed display issues with Creator attributes. Most posts didnt have Creator attribute due to previous issues, causing the error

---
+++ src/components/shared/PostCard.tsx
@@ -0,0 +1,48 @@
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+
+type PostCardProps = {
+    post: Models.Document;
+}
+
+const PostCard = ({post}: PostCardProps) => {
+  return (
+    <div className="post-card">
+        <div className="flex-between">
+            <div className="flex items-center gap-3">
+            <Link to={`/profile/${post?.Creator.$id}`}>
+            <img 
+              src={post?.Creator?.imageURL || 'assets/icons/profile-placeholder.svg'} 
+              className="rounded-full w-12 lg:h-12"
+            />
+            </Link>
+
+            <div className="flex flex-col ">
+              <p className="base-medium lg:body-bold text-light-1">
+                {post.Creator.name}
+              </p>
+              <div className="flex-enter gap-2 text-light-3">
+                <p className="subtle-semibold lg:small-regular">
+                  {post.$createdAt}
+                </p>
+                -
+                <p className="subtle-semibold lg:small-regular">
+                  {post.location}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default PostCard
+
+{/* <Link to={`/profile/${post.creator.$id}`}>
+                
+                </Link> */}
+{/* <img 
+                src={post?.creator.imageURL || 'assets/images/profile-placeholder.svg'}
+                alt="creator"
+             /> */}
