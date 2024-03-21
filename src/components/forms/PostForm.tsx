25a55896cdc34e2b71a4f2d6dfdd2f869679b90e
Isaak <isaac@thesheridans.email>
2024-03-21 13:41:06 -0400

    added update post requirements

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -17,7 +17,7 @@ import { Models } from "appwrite"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
 import { useNavigate } from "react-router-dom"
-import { useCreatePost } from "@/srclib/react-query/queriesAndMutations"
+import { useCreatePost, useUpdatePost } from "@/srclib/react-query/queriesAndMutations"
 
 
 type PostFormProps = {
@@ -27,6 +27,8 @@ type PostFormProps = {
 
 const PostForm = ({ post, action }: PostFormProps) => {
     const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost()
+    const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost()
+    
     const { user } = useUserContext();
     const { toast } = useToast();
     const navigate = useNavigate()
@@ -42,6 +44,20 @@ const PostForm = ({ post, action }: PostFormProps) => {
     })
 
     async function onSubmit(values: z.infer<typeof PostValidation>){
+        if(post && action === 'Update'){
+            const updatedPost = await updatePost({
+             ...values,
+                postId: post.$id,
+                imageId: post?.imageId,
+                imageUrl: post?.imageUrl,
+            })
+            if(!updatedPost){
+                toast({title: 'Please try again'})
+            }
+
+            return navigate(`/posts/${post.$id}`)
+        }
+
          const newPost = await createPost({
              ...values,
              userId: user.id,
@@ -141,8 +157,10 @@ const PostForm = ({ post, action }: PostFormProps) => {
                     <Button 
                         type="submit"
                         className="shad-button_primary whitespace-nowrap"
+                        disabled={isLoadingCreate || isLoadingUpdate}
                     >
-                         Submit
+                         {isLoadingCreate || isLoadingUpdate && 'Loading...'}
+                         {action} Post
                     </Button>
                 </div>
                 
