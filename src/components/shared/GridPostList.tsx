2d139a90fad5c4613f92f3e52f89335228d6babe
Isaak <isaac@thesheridans.email>
2024-03-24 21:47:37 -0400

    created explore and infinite scroll

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { useUserContext } from '@/context/AuthContext'
+import { Models } from 'appwrite'
+import { Link } from 'react-router-dom'
+import PostStats from './PostStats';
 
-const GridPostList = () => {
+type GridPostListProps = {
+  posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
+}
+
+const GridPostList = ({ posts, showUser = true, showStats = true }: GridPostListProps) => {
+  const { user } = useUserContext();
+
   return (
-    <div>GridPostList</div>
+    <ul className='grid-container'>
+      {posts.map((post) => (
+        <li key={post.$id} className='relative min-w-80 h-80'>
+          <Link to={`/posts/${post.$id}`} className="grid-post_link">
+            <img 
+            src={post.imageURL}
+            alt="post"
+            className='h-full w-full object-cover'
+            />
+          </Link>
+
+          <div className='grid-post_user'>
+            {showUser && (
+              <div className='flex items-center justify-start gap-2 flex-1'>
+                <img 
+                src={post.Creator.imageURL}
+                alt='creator'
+                className='h-8 w-8 rounded-full'
+                />
+                <p className='line-clamp-1'>{post.Creator.name}</p>
+                
+              </div>
+            )}
+            {showStats && <PostStats post={post} user={user.id} />}
+          </div>
+          <p className='line-clamp-2'>{post.caption}</p>
+        </li>
+      ))}
+    </ul>
   )
 }
 
