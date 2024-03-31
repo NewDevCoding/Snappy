d42ea99bc530686297095a32fc94ab42d299f8d9
Isaak <isaac@thesheridans.email>
2024-03-31 12:35:53 -0400

    created all users page

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import Loader from '@/components/shared/Loader'
+import UserCard from '@/components/shared/UserCard'
+import { useToast } from '@/components/ui/use-toast'
+import { useGetUsers } from '@/srclib/react-query/queriesAndMutations'
+
 
 const AllUsers = () => {
+
+  const { toast } = useToast()
+
+  const { data: Creators, isLoading, isError: isErrorCreators} = useGetUsers()
+
+  if(isErrorCreators){
+    toast({title: 'Error. Please try again'})
+
+
+    return
+  }
+
   return (
-    <div>AllUsers</div>
+    <div className='explore-container'>
+      <div className='explore-inner_container'>
+        <h2 className="h3-bold md:h2-bold w-full">All Users</h2>
+        {isLoading && !Creators ? (
+          <Loader/>
+        ):(
+          <ul className='user-grid'>
+              {Creators?.documents.map((creator) => (
+                <li key={creator.$id} className="flex-1 min-w-[200px] w-full  ">
+                  <UserCard user={creator} />
+                </li>
+              ))}
+          </ul>
+        )}
+        
+      </div>
+    </div>
   )
+
 }
 
 export default AllUsers
