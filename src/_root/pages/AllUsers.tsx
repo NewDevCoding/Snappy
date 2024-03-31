7cf356002ea59ed023b0981626765bb0b3c1dadb
Isaak <isaac@thesheridans.email>
2024-03-31 13:38:49 -0400

    test commit

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,14 +1,19 @@
 import Loader from '@/components/shared/Loader'
 import UserCard from '@/components/shared/UserCard'
+import { Input } from '@/components/ui/input'
 import { useToast } from '@/components/ui/use-toast'
-import { useGetUsers } from '@/srclib/react-query/queriesAndMutations'
+import useDebounce from '@/hooks/useDebounce'
+import { useGetUsers, useSearchUsers } from '@/srclib/react-query/queriesAndMutations'
+import { useState } from 'react'
 
 
 const AllUsers = () => {
-
+  const [searchTerm, setSearchTerm] = useState('')
+  const debouncedValue = useDebounce(searchTerm, 500)
   const { toast } = useToast()
-
+  const { data: searchedUsers, isFetching: isSearchFetching} = useSearchUsers(debouncedValue)
   const { data: Creators, isLoading, isError: isErrorCreators} = useGetUsers()
+  
 
   if(isErrorCreators){
     toast({title: 'Error. Please try again'})
@@ -17,10 +22,30 @@ const AllUsers = () => {
     return
   }
 
+  const shouldShowSearchResults = searchTerm !== '';
+  
+
   return (
     <div className='explore-container'>
       <div className='explore-inner_container'>
         <h2 className="h3-bold md:h2-bold w-full">All Users</h2>
+
+        <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
+          <img 
+          src='/assets/icons/search.svg'
+          alt='search'
+          height={24}
+          width={24}
+          />
+          <Input 
+          type='text'
+          placeholder='Search'
+          className='explore-search'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+
         {isLoading && !Creators ? (
           <Loader/>
         ):(
